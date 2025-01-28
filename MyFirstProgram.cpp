cceabafa8e1a4288244b5e77d53f955beb139311
the-one-samjones <theonesamjones@gmail.com>
2025-01-28 14:38:20 -0600

    MyFirstProgram

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int numBedrooms = 3;
+    float percentInterest = 0.03;
+    double avgDogLifespan = 10.35;
+    char resetButton = 'r';
+    bool wearingSocks = false;
+    string myString = "Hello, World!";
+    
+    cout << "Integer: " << numBedrooms << endl;
+    cout << "Float: " << percentInterest << endl;
+    cout << "Double: " << avgDogLifespan << endl;
+    cout << "Character: " << resetButton << endl;
+    cout << "Boolean: " << wearingSocks << endl;
+    cout << "String: " << myString << endl;
+    
+    return 0;
+}
